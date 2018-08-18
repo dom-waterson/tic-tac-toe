10faa189d6672c9fa919be6c1c9135ab0956c700
Dominic Waterson <dom_waterson@hotmail.com>
2018-08-18 12:12:23 +0100

    finished react tutorial

---
+++ src/square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default (props) => {
+    return (
+        <button
+            className="square"
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    );
+}
